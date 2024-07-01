0e8aadd8591261196d9be94b57ee99e6208a3737
Chloe.Huanggit config --global user.email huangdanjie07@gmail.com <huangdanjie07@gmail.com>
2024-07-01 11:07:10 +0900

    update 20Lab

--- 20Lab_v2/main.tf
+++ 20Lab_v2/main.tf
@@ -1,27 +1,27 @@
-resource "azurerm_resource_group" "appgrp" {
-  name     = "app-grp"
+resource "azurerm_resource_group" "containergrp" {
+  name     = "container-grp"
   location = "Japan East"
 }
 
-resource "azurerm_storage_account" "storageaccount0630" {
-  name                     = "storageaccount0630"
-  resource_group_name      = azurerm_resource_group.appgrp.name
-  location                 = azurerm_resource_group.appgrp.location
+resource "azurerm_storage_account" "storageaccount0701" {
+  name                     = "storageaccount0701"
+  resource_group_name      = azurerm_resource_group.containergrp.name
+  location                 = azurerm_resource_group.containergrp.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  depends_on = [ azurerm_resource_group.appgrp ]
+  depends_on = [ azurerm_resource_group.containergrp ]
 }
 
 resource "azurerm_storage_container" "data" {
   name                  = "data"
-  storage_account_name  = azurerm_storage_account.storageaccount0630.name
+  storage_account_name  = azurerm_storage_account.storageaccount0701.name
   container_access_type = "blob"
-  depends_on = [ azurerm_storage_account.storageaccount0630 ]
+  depends_on = [ azurerm_storage_account.storageaccount0701 ]
 }
 
 resource "azurerm_storage_blob" "maintf" {
   name                   = "main.tf"
-  storage_account_name   = azurerm_storage_account.storageaccount0630.name
+  storage_account_name   = azurerm_storage_account.storageaccount0701.name
   storage_container_name = azurerm_storage_container.data.name
   type                   = "Block"
   source                 = "main.tf"
